af1d113ab77e42a5b9af442e97d383422da6d140
Kalaychelvam <chelvam86@gmail.com>
2025-09-24 16:59:26 +0530

    Few more changes added in report and testcases

--- src/test/java/tests/Testcase01_Login.java
+++ src/test/java/tests/Testcase01_Login.java
@@ -9,10 +9,12 @@ import pages.LoginPage;
 
 public class Testcase01_Login extends BaseTestCases{
 	
-	@Test(dataProvider = "getData")
+	@Test(dataProvider = "getData", enabled = false)
 	public void loginTestcase(HashMap<String, String> input){
 				
 		LoginPage login = new LoginPage(driver);
+		System.out.println(input.get("username"));
+		System.out.println(input.get("password"));
 		login.enterUsername(input.get("username"))
 		.enterPassword(input.get("password"))
 		.loginSubmitButton();
